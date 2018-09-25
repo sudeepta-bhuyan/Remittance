08ff301dbc3568e442c47fa732e3edf29856ef93
sudeepta-bhuyan <sudeepta29@gmail.com>
2018-09-25 10:30:22 +0530

    addressed review comments, UI changes not done yet to reflect contract changes

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,43 +1,39 @@
 pragma solidity ^0.4.20;
-import "./Owned.sol";
+import './Pausable.sol';
 
-contract Remittance is Owned {
-    address public exchAddr;
-    address public solver;
-    bytes32 public puzzleSolution;
-    bool public releaseFund;
+contract Remittance is Pausable {
     
-    event RemittanceCreated(address by, address exch, uint256 amount);
-    event PuzzleSolved(address by);
-    event FundWithdrawn(address by, uint256 amount);
+    struct Request {
+        address sender;
+        address receiver;
+        uint256 amount;
+        bytes32 puzzleSolution;
+        string notAfter; //Not used
+        bool fundReleased;
+    }
+    mapping(uint256 => Request) requests;
+    uint256 numRequests;
+    
+    event RemittanceRequestCreated(address from, address to, uint256 amount);
+    event RemittanceClaimed(address by, uint256 amount);
 
-    function createRemittance(address exch, bytes32 data) public payable ownerOnly {
-        exchAddr = exch;
-        puzzleSolution = data;
-        emit RemittanceCreated(owner, exchAddr, msg.value);
+    function submitRemittanceRequest(address exch, bytes32 data, string date) public payable onlyIfRunning returns (uint256 requestID) {
+        requestID = numRequests++;
+        requests[requestID] = Request(msg.sender, exch, msg.value, data, date, false);
+        emit RemittanceRequestCreated(msg.sender, exch, msg.value);
     }
     
-    function solvePuzzle(string puzzleInput) public {
-        require(msg.sender == exchAddr);
+    function claimRemittance(uint256 requestId, string puzzleInput) public onlyIfRunning {
+        Request storage req = requests[requestId];
+        require(req.fundReleased == false);
+        require(msg.sender == req.receiver);
+        
         bytes32 candidateSolution = keccak256(abi.encodePacked(puzzleInput));
-        if (keccak256(abi.encodePacked(puzzleSolution)) 
-            == keccak256(abi.encodePacked(candidateSolution))) {
-            solver = exchAddr;
-            releaseFund = true;
-            emit PuzzleSolved(solver);
-        }
+        require(req.puzzleSolution == candidateSolution);
+        req.fundReleased = true;
+        
+        emit RemittanceClaimed(msg.sender, req.amount);
+        msg.sender.transfer(req.amount);
     }
     
-    function withdrawFund() public {
-        require(msg.sender == solver);
-        require(releaseFund == true);
-
-        releaseFund = false;
-        exchAddr = 0;
-        solver = 0;
-
-        uint256 amount = address(this).balance;
-        msg.sender.transfer(amount);
-        emit FundWithdrawn(msg.sender, amount);
-    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var Owned = artifacts.require("./Owned.sol");
 var Remittance = artifacts.require("./Remittance.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Owned);
   deployer.deploy(Remittance);
 };
